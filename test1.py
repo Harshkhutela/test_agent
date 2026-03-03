84e13475505388e147212f23b330ac3342728ca3
Bhumi Sharma <bhumi.pks127@gmail.com>
2026-03-04 00:53:05 +0530

    test1 commit

---
+++ test1.py
@@ -0,0 +1,25 @@
+# Sample Buggy Program
+
+numbers = [10, 20, 30, 40]
+
+total = 0
+
+for i in range(5):
+    total += numbers[i]
+
+print("Total is:", total)
+
+x = 10
+y = "2"
+
+result = x / y
+print("Division result:", result)
+
+if total > 50
+    print("Large total")
+else
+    print("Small total")
+
+name = input("Enter your name: ")
+
+print("Hello " + namee)
